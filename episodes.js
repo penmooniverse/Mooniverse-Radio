f1dc76363e4e7db2bea4aed4dad28ed9986c3160
penmooniverse <penart05@gmail.com>
2025-03-30 11:16:00 +0700

    Add files via upload

---
+++ episodes.js
@@ -0,0 +1,19 @@
+
+const episodes = [
+  {
+    title: "🎙️ Tập 1: Lời chào từ Mooniverse",
+    id: "1dWSbsd-5lviKhChl8dMV81NEk2GH5-uM"
+  },
+  {
+    title: "🌕 Tập 2: Hành trình ánh sáng - Phỏng vấn anh Pen",
+    id: "YOUR_AUDIO_ID_2"
+  },
+  {
+    title: "✨ Tập 3: Cùng tìm hiểu về vũ trụ Mooniverse",
+    id: "YOUR_AUDIO_ID_3"
+  },
+  {
+    title: "🎵 Tập 4: Ý nghĩa lời bài hát Stayhungry Stay Foolish",
+    id: "YOUR_AUDIO_ID_4"
+  }
+];
